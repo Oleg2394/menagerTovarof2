6f6d67017a9555a990add173516fb3a37db22f5b
Oleg2394 <50140221+Oleg2394@users.noreply.github.com>
2020-06-24 16:55:34 +0700

    Update ProductRepositoryTest.java

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -3,13 +3,14 @@ package ru.netology.repository;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
-
+import ru.netology.domain.Smartphone;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
 
     private ProductRepository repository = new ProductRepository();
     private Book coreJava = new Book();
+    Smartphone smart = new Smartphone(1,"Nokia",1500,"Nokia");
 
     @Test
     public void shouldSaveOneItem() {
@@ -17,5 +18,12 @@ class ProductRepositoryTest {
         Product[] expected = new Product[]{coreJava};
         Product[] actual = repository.findById();
         assertArrayEquals(expected, actual);
+    }
+    
+      @Test
+   public void shouldRemove() {
+        repository.save(smart);
+        repository.removeById(1);
+        assertNull(repository.findById(1));
     }
 }
